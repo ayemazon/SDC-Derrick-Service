2c2d8a467f5c69add2e5c854f37f34ac92340771
creationator <morton.derrick@gmail.com>
2019-09-05 00:34:03 -0400

    Fixed seeding script for one million records

--- database/seed.js
+++ database/seed.js
@@ -3,13 +3,16 @@ const addToDB = require('../database/db').addToDB;
 const seed = () => {
   var galleryImages = [];
 
-  for ( var i = 0; i <= 10000000; i++) {
+  for ( var i = 0; i <= 1000000; i++) {
       galleryImages.push({
         imgid: i,
         url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s1.jpg'
       })
   }
-  return galleryImages;
+  for (var i = 0; i < galleryImages.length; i++) {
+    addToDB(galleryImages[i].imgid, galleryImages[i].imageurl);
+  }
 }
-const seedData = seed();
-addToDB(seedData);
+
+seed();
+
