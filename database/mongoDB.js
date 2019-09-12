5c4dec0f7df03508bf1f7a2c60193eea6d31a5b8
creationator <morton.derrick@gmail.com>
2019-09-11 23:27:42 -0400

    Added seeding script for mongoDB

--- database/mongoDB.js
+++ database/mongoDB.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 mongoose.connect('mongodb://localhost/imageGallery', { useNewUrlParser: true }, (err) => {
   if (err) {
     console.err(err);
@@ -11,7 +10,10 @@ mongoose.connect('mongodb://localhost/imageGallery', { useNewUrlParser: true }, 
 const dbConnect = mongoose.connection;
 
 let imageGallerySchema = mongoose.Schema({
-  imgid: Number,
+  imgid: {
+    type: String,
+    unique: true
+  },
   url: String,
 });
 
@@ -20,12 +22,14 @@ let ImageGalleryDetails = mongoose.model('imageGallery', imageGallerySchema);
 let saveImageUrl = (imageDetails) => {
   ImageGalleryDetails.create(imageDetails, (err, image) => {
     if (err) {
-      console.err(err);
+      console.error(err)
     }
     console.log('Image saved to database');
-  });
+  })
 }
-
+let deleteDB = () => {
+  dbConnect.dropDatabase();
+}
 let findAllImages = () => {
   return ImageGalleryDetails.find({}).exec();
 }
@@ -35,7 +39,34 @@ let deleteAImage = (imgid) => {
     console.log('Image has been deleted.');
   });
 }
-
+var counter = 1;
+let insertIntoMongoDb = () => {
+  if (counter <= 700000) {
+    ImageGalleryDetails.insertMany([
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s1.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s2.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s3.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s4.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s5.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s6.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s7.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s8.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s9.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s10.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s11.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s12.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s13.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s14.jpg' },
+      { url: 'https://rpt14-sdc-derrick.s3.amazonaws.com/images/s15.jpg' }
+    ])
+    counter++;
+    setTimeout(insertIntoMongoDb, 1);
+  }
+}
+// the insertIntoMongoDb is being called 700000  times and each time its adding 15 records into the DB totalling 10.5 million
 module.exports.saveImageUrl = saveImageUrl;
 module.exports.findAllImages = findAllImages;
 module.exports.deleteAImage = deleteAImage;
+module.exports.deleteDB = deleteDB;
+module.exports.insertIntoMongoDb = insertIntoMongoDb;
+// module.exports.insertIntoMongoDb = insertIntoMongoDb;
